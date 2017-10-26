05dd1f258ba01865b015a44e9848ec161e6dde8a
dipeshPandey <dipesh.pandey42@hotmail.com>
2017-10-26 09:02:20 +0545

    Classes added

---
+++ src/main/java/com/Models/BaseClass/channel/ChannelDetails.java
@@ -0,0 +1,24 @@
+package com.Models.BaseClass.channel;
+
+import javax.persistence.*;
+import javax.persistence.metamodel.CollectionAttribute;
+import java.util.Collection;
+
+/**
+ * Created by mepesh on 10/26/2017.
+ */
+@Entity
+public class ChannelDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long channelDetailsId;
+    private String coverage;
+    // one to many mapping for region
+    private String region;
+    private String type;
+    private String Address;
+    private String channelDescription;
+//    @JoinColumn(name="channel_fk")
+    @ManyToOne
+    private Channel channelId;
+}
